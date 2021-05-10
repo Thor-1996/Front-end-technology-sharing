2bafda28c966975d52f597da82451e90dc74a207
Thor <15073245605@163.com>
2021-05-10 18:08:21 +0800

    22

--- express-app/test.js
+++ express-app/test.js
@@ -57,11 +57,35 @@ var arr1 = [
 // arr1里面每一项下的层级都一样，如示例都是三级
 // 转换后期望输出arr2：
 
-var aar2 = [
-  { code1: "1", code2: "11", code3: "111" },
-  { code1: "1", code2: "11", code3: "112" },
-  { code1: "2", code2: "22", code3: "221" },
-  { code1: "2", code2: "22", code3: "222" },
-  { code1: "2", code2: "22", code3: "223" },
-  { code1: "3", code2: "33", code3: "333" },
-];
+// var aar2 = [
+//   { code1: "1", code2: "11", code3: "111" },
+//   { code1: "1", code2: "11", code3: "112" },
+//   { code1: "2", code2: "22", code3: "221" },
+//   { code1: "2", code2: "22", code3: "222" },
+//   { code1: "2", code2: "22", code3: "223" },
+//   { code1: "3", code2: "33", code3: "333" },
+// ];
+
+function getPath(arr = []) {
+  const pathList = [];
+
+  function deep(arr, level, item) {
+    for (let i = 0; i < arr.length; i++) {
+      const obj = {
+        [`code${level}`]: arr[i].code,
+        ...item,
+      };
+
+      if (arr[i].children && arr[i].children.length) {
+        deep(arr[i].children, level + 1, obj);
+      } else {
+        pathList.push(obj);
+      }
+    }
+  }
+
+  deep(arr, 1, {});
+  return pathList;
+}
+
+getPath(arr1);
